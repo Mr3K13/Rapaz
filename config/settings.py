a7f6b78ba7086bc45be67124447135d7a5cdc9af
Marcus Vinicius <125592845+Mr3K13@users.noreply.github.com>
2026-05-06 16:36:57 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # ─── Segurança ────────────────────────────────────────────────────────────────
 SECRET_KEY = config(
     'SECRET_KEY',
-    default=''
+    default='django-insecure-troque-esta-chave-em-producao-!@#$%^&*()'
 )
 
 DEBUG = config('DEBUG', default=False, cast=bool)
