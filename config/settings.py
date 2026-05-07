b8ef6d9a6f51f03f8b395515caea6f261b50e047
Marcus Vinicius <125592845+Mr3K13@users.noreply.github.com>
2026-05-06 23:46:50 -0300

    Update CORS settings to allow all origins
    Removed specific CORS allowed origins and set to allow all origins.

--- config/settings.py
+++ config/settings.py
@@ -112,15 +112,8 @@ REST_FRAMEWORK = {
 }
 
 # ─── CORS ─────────────────────────────────────────────────────────────────────
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:8000',
-]
 CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_HEADERS = [
     "accept",
     "authorization",
@@ -129,4 +122,3 @@ CORS_ALLOW_HEADERS = [
     "x-csrftoken",
     "x-requested-with",
 ]
-CORS_ALLOW_CREDENTIALS = True
