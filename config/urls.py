c5bbc59e2e9cf5cff9ab3b14d7409e17fb01c94c
Marcus Vinicius <ph154522@gmail.com>
2026-05-07 00:47:26 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
-"""Configuração de URLs principal do projeto django-auth-backend."""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
