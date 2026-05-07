3bb5a08831023802d8927cb3cffd32bacc36aa5a
Marcus Vinicius <125592845+Mr3K13@users.noreply.github.com>
2026-05-06 23:36:47 -0300

    Enable CORS for all origins

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:8000',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_HEADERS = [
     "accept",
     "authorization",
