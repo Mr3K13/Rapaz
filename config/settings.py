effca322bb6620c1ae4f0d4ce90490edd4c97956
Marcus Vinicius <125592845+Mr3K13@users.noreply.github.com>
2026-05-28 15:08:52 -0300

    Link att

--- config/settings.py
+++ config/settings.py
@@ -104,6 +104,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://adocao-projeto.vercel.app/",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
